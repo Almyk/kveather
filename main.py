03fc5b914f10d4af58561e5ba94a692a91da7762
almyk <hansentommy92@gmail.com>
2018-06-04 23:04:39 +0900

    Add location storage

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ from kivy.properties import ObjectProperty, ListProperty, StringProperty, Numeri
 from kivy.network.urlrequest import UrlRequest
 from kivy.uix.listview import ListItemButton
 from kivy.factory import Factory
+from kivy.storage.jsonstore import JsonStore
 
 f = open('api.key', 'r')
 key = f.readline().strip()
 f.close()
 
+def locations_args_converter(index, data_item):
+    col = '#2980b9'
+    if index % 2:
+        col = '#3498db'
+    city, country = data_item
+    return {'location': (city, country), 'col': col}
 
 class AddLocationForm(BoxLayout):
     search_input = ObjectProperty()
@@ -26,14 +33,8 @@ class AddLocationForm(BoxLayout):
         self.search_results.item_strings = cities
         self.search_results.adapter.data.clear()
         self.search_results.adapter.data.extend(cities)
-        self.search_results._trigger_reset_populate()
+        #self.search_results._trigger_reset_populate()
 
-    def args_converter(self, index, data_item):
-        col = '#2980b9'
-        if index % 2:
-            col = '#3498db'
-        city, country = data_item
-        return {'location': (city, country), 'col': col}
 
 class LocationButton(ListItemButton):
     location = ListProperty()
@@ -42,15 +43,17 @@ class LocationButton(ListItemButton):
 class CurrentWeather(BoxLayout):
     location = ListProperty(['New York', 'US'])
     conditions = StringProperty()
+    conditions_image = StringProperty()
     temp = NumericProperty()
     temp_min = NumericProperty()
     temp_max = NumericProperty()
-    conditions_image = StringProperty()
 
     def update_weather(self):
+        config = WeatherApp.get_running_app().config
+        temp_type = config.getdefault("General", "temp_type", "metric").lower()
         weather_template = \
-            "http://api.openweathermap.org/data/2.5/weather?q={},{}&units=metric&APPID="+key
-        weather_url = weather_template.format(*self.location)
+            "http://api.openweathermap.org/data/2.5/weather?q={},{}&units={}&APPID="+key
+        weather_url = weather_template.format(*self.location, temp_type)
         request = UrlRequest(weather_url, self.weather_retrieved)
 
     def weather_retrieved(self, request, data):
@@ -61,29 +64,80 @@ class CurrentWeather(BoxLayout):
         self.temp_min = data['main']['temp_min']
         self.temp_max = data['main']['temp_max']
 
-class WeatherApp(App):
-    pass
 
 class WeatherRoot(BoxLayout):
     current_weather = ObjectProperty()
+    locations = ObjectProperty()
     add_location_form = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(WeatherRoot, self).__init__(**kwargs)
+        self.store = JsonStore("weather_store.json")
+        self.add_location_form = Factory.AddLocationForm()
+        try:
+            current_location = self.store.get("locations")['current_location']
+            self.show_current_weather(current_location)
+        except:
+            self.show_add_location_form()
 
     def show_current_weather(self, location=None):
         self.clear_widgets()
 
         if self.current_weather is None:
             self.current_weather = CurrentWeather()
+        if self.locations is None:
+            self.locations = Factory.Locations()
+            if(self.store.exists('locations')):
+                locations = self.store.get("locations")['locations']
+                self.locations.locations_list.adapter.data.extend(locations)
 
         if location is not None:
             self.current_weather.location = location
+            if location not in self.locations.locations_list.adapter.data:
+                self.locations.locations_list.adapter.data.append(location)
+                #self.locations.locations_list._trigger_reset_populate()
+                self.store.put("locations",
+                        locations=list(self.locations.locations_list.adapter.data),
+                        current_location=location)
+
         self.current_weather.update_weather()
         self.add_widget(self.current_weather)
     
     def show_add_location_form(self):
         self.clear_widgets()
-        if self.add_location_form is None:
-            self.add_location_form = AddLocationForm()
         self.add_widget(self.add_location_form)
+
+    def show_locations(self):
+        if self.locations is None:
+            self.locations = Factory.Locations()
+            if(self.store.exists('locations')):
+                locations = self.store.get("locations")['locations']
+                self.locations.locations_list.adapter.data.extend(locations)
+
+        self.clear_widgets()
+        self.add_widget(self.locations)
+
+class WeatherApp(App):
+    def build_config(self, config):
+        config.setdefaults('General', {'temp_type': "Metric"})
+
+    def build_settings(self, settings):
+        settings.add_json_panel("Weather Settings", self.config, data="""
+            [
+                {"type": "options",
+                    "title": "Temperature System",
+                    "section": "General",
+                    "key": "temp_type",
+                    "options": ["Metric", "Imperial"]
+                }
+            ]"""
+            )
+    def on_config_change(self, config, section, key, value):
+        if config is self.config and key == "temp_type":
+            try:
+                self.root.children[0].update_weather()
+            except AttributeError:
+                pass
 
 if __name__ == '__main__':
     WeatherApp().run()
